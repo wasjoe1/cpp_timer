fbee167eb7ad623fa503ad6d7680cd9ae9679c8b
joe_mac <joechua247@gmail.com>
2026-04-08 12:20:08 +0800

    create working timer

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+// TODO: write a 5 sec timer, which alerts when time ends
+
+#include <iostream>  // for std::cout
+#include <thread>  // for threading
+#include <chrono> // for time?
+
+void sleep_wait(int duration) {
+    // empty task that needs to be done => 
+    std::cout << "Start 10s task..." << std::endl;
+    // sleep for 10s
+    std::this_thread::sleep_for(std::chrono::seconds(10));
+    std::cout << "10s task ended!" << std::endl;
+}
+
+int main() {
+    // set a timer
+    // do_something
+    // timer ends & alerts
+    // finishes program even before do_something can end
+
+    // run 10s task in separate detached thread
+    // 5 second timer will be here in main thread
+    std::thread worker_thread{sleep_wait, 10};
+
+    worker_thread.detach();
+
+    std::cout << "5 second timer started..." << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(5));
+    std::cout << "5 second timer ended! closing all other unfinished threads" << std::endl;
+}
+
+// 1. (issue)
+    /*
+    currently there are many occasions where 5s timer starts b4 sleep_wait executed
+    this is not intended as we executed the timer first
+    likely reasons of failure:
+        a. concurrency/ race condition
+        b. std::cout's buffer
+    => resolve this issue!
+    */
+
+// 2. (another implementation)
+    /*
+    we want to also be able to create a timer for busy task; implent busy_wait below
+    (we will need an alert to context switch out of the busy task)
+    */
+
+
+void busy_wait() {
+    // empty task that needs to be done => CPU heavy
+}
